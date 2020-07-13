c4059a1300e0467ba694c0278c46ab6eb66610df
Doug Raum <draum@wirelessanalytics.com>
2020-07-13 11:24:07 -0400

    Add example assessment getter

--- src/RESTAdapterMain.cpp
+++ src/RESTAdapterMain.cpp
@@ -747,7 +747,7 @@ private:
     void getAssessment(const Rest::Request &request, Http::ResponseWriter response) {
         std::string name = request.param(":name").as<std::string>();
         std::string fileName = "assessments/" + name;
-        LOG_INFO << "Recieved a request to GET an assessment file, so we're serving this up: " fileName;
+        LOG_INFO << "Recieved a request to GET an assessment file, so we're serving this up: " << fileName;
         Http::serveFile(response, fileName.c_str());
     }
 
